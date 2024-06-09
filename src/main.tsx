c3c7f527cdf35cb951f15fb8828a3f3069438cdc
Manos <igoufe.osman@gmail.com>
2024-06-09 13:09:18 -0400

    Maj des App.tsx / Index.css et main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {ChakraProvider, extendTheme} from '@chakra-ui/react'
 import App from './App.tsx'
 import './index.css'
-import {ChakraProvider, extendTheme} from '@chakra-ui/react'
 
 const colors = {
     brand: {
-        900: '#1a365d',
-        800: '#153e75',
-        700: '#2a69ac',
+        900: '#5d1a22',
+        800: '#2a7515',
+        700: '#9b2aac',
     },
 }
 
@@ -19,3 +19,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <App/>
     </ChakraProvider>
 </React.StrictMode>,)
+
+
