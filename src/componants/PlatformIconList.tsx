d4952b40fd0429b9f908e3fe4bc4d8223522b9fa
Manos <igoufe.osman@gmail.com>
2024-06-10 17:34:24 -0400

    Add platform icons to GameCard
    A new component, PlatformIconList, has been created to display platform-specific icons on the GameCard. The "react-icons" package was installed to facilitate this, and a "Platform" interface has been added to "useGames.tsx" to handle platform data. This will improve the visual representation of each game's supported platforms, contributing to better user experience.

---
+++ src/componants/PlatformIconList.tsx
@@ -0,0 +1,29 @@
+import {FaWindows, FaPlaystation, FaApple, FaXbox, FaLinux} from "react-icons/fa";
+import {MdPhoneIphone} from "react-icons/md";
+import {SiNintendo} from "react-icons/si";
+import {BsGlobe} from "react-icons/bs";
+import {Platform} from "../hooks/useGames.tsx";
+import {HStack, Icon} from "@chakra-ui/react";
+import {IconType} from "react-icons";
+
+interface Props {
+	platforms: Platform[];
+}
+
+function PlatformIconList ( {platforms}: Props ) {
+	const iconMap: { [key: string]: IconType } = {
+		pc: FaWindows, xbox: FaXbox, playstation: FaPlaystation, linux: FaLinux, apple: FaApple, nintendo: SiNintendo, ios: MdPhoneIphone, web: BsGlobe,
+	};
+	return (
+		<HStack margin={1}>
+			{
+				platforms.map( ( platform ) => (
+						<Icon as={iconMap[platform.slug]} color="gray.500"/>
+					),
+				)
+			}
+		</HStack>
+	);
+}
+
+export default PlatformIconList;
