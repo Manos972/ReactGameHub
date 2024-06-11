ffcd9726268a181511801fc0f9e9cb9e55d8d848
Manos <igoufe.osman@gmail.com>
2024-06-10 21:56:49 -0400

    Add loading spinner and error handling in GenresList component
    A Spinner from Chakra UI library was included and used in the GenresList component to visually indicate the data loading process. Moreover, error handling was improved by returning null if an error occurs during data fetching.

--- src/componants/GenresList.tsx
+++ src/componants/GenresList.tsx
@@ -1,10 +1,12 @@
-import {Text, List, ListItem, HStack, Image} from "@chakra-ui/react";
+import {Text, List, ListItem, HStack, Image, Spinner} from "@chakra-ui/react";
 import useFetch from "../hooks/useFetchs.ts";
 import {Genre} from "../hooks/useGenres.tsx";
 import getCropImageUrl from "../services/image-url.ts";
 
 function GenresList () {
-	const {data, error} = useFetch<Genre>( "/genres" );
+	const {data, error, loading} = useFetch<Genre>( "/genres" );
+	if ( error ) return null;
+	if ( loading ) return <Spinner/>;
 	return (
 		<>
 			<List>{error && <Text>{error}</Text>}
